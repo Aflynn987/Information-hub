7aa213a86d66fb928789e6d5d6765447a5618633
Aflynn987 <aflynn987@gmail.com>
2023-05-11 05:13:27 +0100

    remove unused scripts

--- info_hubs/test_script.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://www.rte.ie/news/business/"
-response = requests.get(url)
-if response.status_code == 200:
-    print(response.text)
-else:
-    print("Error: Unable to retrieve HTML content from URL")
