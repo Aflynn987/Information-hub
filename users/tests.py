dd207e6f9620e8e3b790269dc820a637df0ab986
Aflynn987 <aflynn987@gmail.com>
2023-05-14 15:37:39 +0100

    add tests

--- users/tests.py
+++ users/tests.py
@@ -13,20 +13,21 @@ class TestAuthViews(TestCase):
             email='testuser@example.com',
             password='testpass'
         )
+        self.link = "http://ec2-13-50-14-60.eu-north-1.compute.amazonaws.com:3000"
 
     def test_logout_view(self):
         self.client.login(username='testuser', password='testpass')
-        response = self.client.get(reverse('logout'))
+        response = self.client.get((self.link+'/users/logout/'))
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse('index'))
+        self.assertRedirects(response, '/')
         self.assertFalse('_auth_user_id' in self.client.session)
 
     def test_register_view_get(self):
-        response = self.client.get(reverse('register'))
+        response = self.client.get((self.link+'/users/register/'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/register.html')
-        self.assertContains(response, '<h2>Register</h2>')
-        self.assertContains(response, '<input type="submit"')
+        self.assertContains(response, '<a href="/users/register/">Register</a>')
+        self.assertContains(response, '<button name="submit">register</button>')
 
     def test_register_view_post_valid(self):
         form_data = {
@@ -34,9 +35,9 @@ class TestAuthViews(TestCase):
             'password1': 'newpass123',
             'password2': 'newpass123'
         }
-        response = self.client.post('http:/127.0.0.1:8000/users/register/', form_data)
+        response = self.client.post((self.link+'/users/register/'), form_data)
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse('index'))
+        self.assertRedirects(response, '/')
         user = authenticate(username='newuser', password='newpass123')
         self.assertIsNotNone(user)
         self.assertEqual(user.username, 'newuser')
@@ -47,7 +48,6 @@ class TestAuthViews(TestCase):
             'password1': 'testpass',
             'password2': 'testpass'
         }
-        response = self.client.post(reverse('register'), form_data)
+        response = self.client.post((self.link+'/users/register/'), form_data)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/register.html')
-        self.assertContains(response, 'This username is already taken')
